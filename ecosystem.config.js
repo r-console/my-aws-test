b4de18e516ea50d372dbded0f258597b82d98405
r-console <rameshsolaimalai878@gmail.com>
2022-08-02 15:20:34 +0530

    pm2 start

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,10 @@
 module.exports = {
   apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+    name:"app",
+    script: 'app.js',
+    watch: '.',
+    instances : "max",
+    exec_mode : "cluster",
+    out_file:"./ec2.log",
+  }],  
 };
